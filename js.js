3a1e57392597633837adbb64c2cf8be99dbcf045
NiklasThorstensen <2000958@apps.nsd.org>
2025-06-05 16:26:48 +0000

    excellence

--- js.js
+++ js.js
@@ -1,43 +1,108 @@
 
 var LIST = [
-    [
-        "someone",
-        "whoever",
-        "dude what"
-    ],
-    [
-        "someone"
-    ],
-    [
-        "huh"
-    ]
+    {
+        title:"Back in Mid-May, 2 people fought over some...",
+        class:"f1",
+        chrs:[
+            "mainchar",
+            "rival",
+            "flintandsteel",
+            "deadguy"
+        ],
+        words:[
+            "Flint and Steel!",
+            "No its mine!!!!!!!!",
+            "I'm flint and steeling right now",
+            "i am ded"
+        ]
+    },
+    {
+        title:"And whoever this guy is won, but he also picked up...",
+        class:"f2",
+        chrs:[
+            "mainchar",
+            "AnEnderPearl"
+        ],
+        words:[
+            "An Ender Pearl.",
+            "???????"
+        ]
+    },
+    {
+        title:"And he ends up finding an already finished portal to...",
+        class:"f3",
+        chrs:[
+            "mainchar",
+            "AnEnderPearl"
+        ],
+        words:[
+            "G",
+            "blanktext"
+        ]
+    },
+    {
+        title:"Uhhh... Dude?",
+        class:"f4",
+        chrs:[  
+            "nether"
+        ],
+        words:[
+            "Nope, nevermind, im not doing this"
+        ]
+    },
+    {
+        title:"the_end",
+        class:"f5",
+        chrs:[
+            "theEnd"
+        ],
+        words:[
+            "The End"
+        ]
+    }
 ];
 
 var dance = function(x, dunno){
-    var blocks =  document.getElementById(x);
-    for(var i = 0; i < dunno; i++){
+
+    var blocks =  document.querySelector(x);
+
+    for(var i = 0; i < dunno.length; i++){
         var placing = blocks.cloneNode(true);
+        placing.innerHTML = dunno[i].title;
+        placing.classList.add(dunno[i].class);
+        placing.classList.add("blocks");
+
+        for(var c = 0; c< dunno[i].chrs.length; c++){
+            var chrHTML = document.querySelector("#placers .chr").cloneNode(true);
+            var talkHTML = chrHTML.querySelector(".talk");
+            //chrHTML.innerHTML = dunno[i].chrs[c];
+            chrHTML.classList.add(dunno[i].chrs[c]);
+            chrHTML.classList.add("chr");
+            talkHTML.innerHTML = dunno[i].words[c];
+
+            placing.append(chrHTML);
+        }
+
         document.body.append(placing);
     }
 }
 
-dance("placing", 7);
+dance("#placers .frame", LIST);
 
 var inHtml = function(x){
     document.querySelector("div#innerHTMLFUN").innerHTML = x;
 }
-inHtml("EYYAAAAAAAAAXXXXXX");
-
+inHtml("I have no idea what i am even doing here anymore or what to type here so 1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()QWERTYUIOPASDFGHKLZXCVBNM123l");
 
 var draw = function(x){
-    var target = document.getElementById(hitme);
+    var target = document.getElementById(target);
     var stuffshowin = "";
     for(
         var count = 0;
         count < x.length;
         count++
     ){
-        alert("!!!!!!!!!FUREMU!!!!!!!!!");
+        alert("!!!!!!!!!FRAME!!!!!!!!!");
         var nextList = x[count]
         for(var i = 0; i < nextList.length; i++){
             alert(nextList[i]);
