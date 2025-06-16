ba0b0755f7024c0e3653789c85956d279a2006a1
NiklasThorstensen <2000958@apps.nsd.org>
2025-06-16 15:43:41 +0000

    modified:   js.js

--- js.js
+++ js.js
@@ -94,7 +94,7 @@ dance("#placers .frame", LIST);
 var inHtml = function(x){
     document.querySelector("div#innerHTMLFUN").innerHTML = x;
 }
-inHtml("A story of some green dude under some brainrot vwhile playing Minecraft");
+inHtml("A really short story of some green dude under some brainrot while playing Minecraft, that's all there is");
 
 var draw = function(x){
     var target = document.getElementById(target);
